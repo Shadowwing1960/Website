c6233d49b69ff767a873860246761953db75378c
Shadowwing1960 <98994815+Shadowwing1960@users.noreply.github.com>
2022-08-01 11:38:00 +1200

    Add files via upload

---
+++ views.py
@@ -0,0 +1,65 @@
+from flask import Blueprint, render_template, request, flash, jsonify, redirect, url_for
+from flask_login import login_required, current_user
+from Web.Website.models import Note
+from Web.Website import db
+import json
+import threading
+import sys
+
+
+
+views = Blueprint('views', __name__)
+
+
+@views.route('/', methods=['GET', 'POST'])
+@login_required
+def home():
+    return render_template("home.html", user=current_user)
+
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    note = json.loads(request.data)
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+
+    return jsonify({})
+
+
+@views.route('/notepad')
+def notepad():
+    def redirect_home():
+        redirect(url_for('/'))
+
+    # Specific path
+    path = 'C:/Users/HP Admin/PycharmProjects/Website/Notes/Notes_Code'
+    sys.path.insert(0, path)
+
+    # importing function
+    from Notes.Notes_Code.Pad_Code import note_pad
+    notes = threading.Thread(target=note_pad, name='notes')
+    redirecting= threading.Thread(target=redirect_home, name='redirecting')
+    # Running Gui Note Pad
+    redirecting.start()
+    notes.start()
+    return render_template("home.html", user=current_user)
+
+
+@views.route('/todolist', methods=['GET', 'POST'])
+def to_do_list():
+    if request.method == 'POST':
+        note = request.form.get('note')
+
+        if len(note) < 1:
+            flash('Task is too short!', category='error')
+        else:
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Task added!', category='success')
+
+    return render_template("to_do_list.html", user=current_user)
